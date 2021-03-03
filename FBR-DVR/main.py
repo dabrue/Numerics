794512b69bb2e294bd94adad0fff60fda5761781
Daniel Brue <danielbrue gmail>
2021-03-02 22:25:45 -0600

    Initial Project commit

---
+++ FBR-DVR/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+This is a practice script for FBR/DVR 
+
+Finite Element Representation to Discrete Variable Representation
+'''
+import numpy as np
+import scipy as sp
+import scipy.special as sps
+import matplotlib.pyplot as plt
+import pandas
+
+if (__name__ == '__main__'):
+
+
+    # First let us see how calling the built-in special functions works
