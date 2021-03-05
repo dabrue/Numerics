d4559cbfad6c5c7cda74fee17279d1a4a61774c5
Daniel Brue <danielbrue gmail>
2021-03-04 18:59:54 -0600

    more work on daf and dvr, begin dvr documentation.

--- DAF/DAF.py
+++ DAF/DAF.py
@@ -2,17 +2,46 @@
 '''
 Module providing routines for Distributed Approximating Functionals numerical methods
 
-D. A. Brue, 2021
+Daniel Brue, 2021
 '''
-import numpy
-import scipy
 
-class daf_Hermite:
+import numpy as np
+import scipy as sp
+import scipy.special as sps
 
-    def __init__(self, Order, SumOrder=50, sigma=1.0):
-        self.sigma = sigma
-        self.SumOrder = SumOrder
-        self.Order = Order
+def gen_Hermite(Xray, M, sigma, DerOrder):
+    pass
 
-    def _gen_DAF_matrix(order,xpts,weights):
-        pass
+def gen_Legendre(Xray, M, DerOrder):
+    pass
+
+def gen_Laguerre(Xray, M, sigma, DerOrder):
+    pass
+
+
+class DAF:
+
+    def __init__(PolyBase='hermite':str,Xray:np.array,DerOrder=0:int,sigma=1.0:float,M=50:int):
+        self.PolyBase = PolyBase
+        self.Xray = Xray
+        self.DerOrder = DerOrder
+        self.sigma=1.0
+        self.ExpOrder = M           
+        if (PolyBase == 'hermite'):
+            DAFMAT = gen_Hermite(Xray, M 
+        elif (PolyBase == 'legendre'):
+            DAFMAT = gen_Legendre(Xray, M)
+        elif (PolyBase == 'laguerre'):
+            DAFMAT = gen_Laguerre(Xray, M, sigma)
+        elif (PolyBase == 'chebyshev'):
+            DAFMAT = gen_Chebyshev(Xray, M
+        else:
+            raise('ERROR: Unidentified Polynomial Expansion : ', PolyBase)
+            exit()
+
+if (__name__ == '__main__'):
+    import re
+    import random
+    import matplotlib.pyplot as plt
+    
+    
