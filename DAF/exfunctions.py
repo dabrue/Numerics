e671efad02fad6b0ac268c7561f68f3537a41b91
Daniel Brue <danielbrue gmail>
2021-03-10 21:02:58 -0600

    DAF progressing

---
+++ DAF/exfunctions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This routine generates test functions for studying the DAF methods
+
+The whole point is to break the DAF really. 
+'''
+import numpy as np
+import scipy as sp
+
+if (__name__ == '__main__'):
+    import matplotlib.pyplot as plt
