e8b5486b26bf669c5db91c3019fba60da5abc2d8
Daniel Brue <danielbrue+git@gmail.com>
2021-03-24 12:31:05 -0500

    Merging changes.

---
+++ Djikstra/dijkstra.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+This routine implements and tests variations of the Djikstra algorithm for 
+minimizing or maximizing a path based off of given or calculated line and 
+node values in an acyclic directed graph. 
+
+Daniel A. Brue, 2021
+'''
+import math
+import numpy as np
